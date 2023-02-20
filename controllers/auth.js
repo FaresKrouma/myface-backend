909d9f1d0055b5ba5d41390863b782f3dcdfd287
Fares Krouma <88767998+FaresKrouma@users.noreply.github.com>
2023-02-20 22:21:59 +0100

    added some comments

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,16 @@
 const login = (req, res) => {
   res.json("login");
+  // check if user exists , if not send error
+
+  // check if user exists, if so check details
 };
 
 const register = (req, res) => {
   res.json("register");
+  // check if user exists , if so send error (user exists already)
+
+  // if no user found , create user
+  // hash password
 };
 
 const logout = (req, res) => {
